48395e3abad3bf922d22d6a64bae18f97197d3cf
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-04-24 18:58:55 +0300

    bump version → 1.0.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="vedro-valera-validator",
-    version="0.1.3",
+    version="1.0.0",
     description="Validator for Vedro framework",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
