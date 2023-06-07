2805d6662edadc224dd638a0aeee01d368560abd
Nikita Tsvetkov <tsv1@fastmail.com>
2023-06-07 19:16:58 +0400

    add project_urls

--- setup.py
+++ setup.py
@@ -14,13 +14,17 @@ def find_dev_required():
 setup(
     name="vedro-valera-validator",
     version="1.1.1",
-    description="Validator plugin for the Vedro testing framework",
+    description="Validator plugin for Vedro testing framework",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Nikita Tsvetkov",
     author_email="tsv1@fastmail.com",
     python_requires=">=3.8",
     url="https://github.com/vedro-universe/vedro-valera-validator",
+    project_urls={
+        "Docs": "https://vedro.io/docs/integrations/valera-validator",
+        "GitHub": "https://github.com/vedro-universe/vedro-valera-validator",
+    },
     license="Apache-2.0",
     packages=find_packages(exclude=["tests", "tests.*"]),
     package_data={"vedro_valera_validator": ["py.typed"]},
